76aaca89721b95ee1dc214e1fe21e3e2298a6be4
Himanshu Itoria <itoriahimanshu@gmail.com>
2021-05-31 22:45:04 +0530

    Complete portfolio backend done

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,13 @@ class Blog(models.Model):
      title = models.CharField(max_length = 200)
      body = models.TextField()
      published_date = models.DateTimeField()
+
+
+     def __str__(self):
+         return self.title
+
+     def summary(self):
+         return self.body[:100]
+
+     def published_date_pretty(self):
+         return self.published_date.strftime('%b %e %Y')
